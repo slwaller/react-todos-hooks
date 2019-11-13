92865141fb9a124e3ba4cd1d8a985a0247be294a
Sam Waller <samuellwaller@gmail.com>
2019-11-13 17:58:21 -0500

    add todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
+import './App.css' 
 
 function Todo({ todo, idx}) {
   return(
     <div className="todo">
       { todo.text }
     </div>
+  )
+}
+
+function TodoForm({addTodo}) {
+  const [val, setVal] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!val) return;
+    addTodo(val);
+    setVal('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}> 
+      <input type="text" placeholder="Add Todo" className="input" value={val} onChange={e => setVal(e.target.value)} />
+    </form>
   )
 }
 
@@ -24,6 +42,11 @@ function App(){
     }
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }]
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
@@ -34,6 +57,9 @@ function App(){
               todo={todo}
             />
           ))}
+          <TodoForm 
+            addTodo={addTodo}
+          />
       </div>
     </div>
   )
